11a50cb3a2c5bc93a7e8ad5f39966029f7d86f5b
meozalat2022 <meozalat2002@hotmail.com>
2024-05-06 22:56:57 +0300

    creating models

---
+++ api/models/message.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  senderId: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+  recipientId: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+  messageType: {
+    type: String,
+    enum: ["text", "image"],
+  },
+  message: String,
+  timeStamp: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
---
+++ api/models/user.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  friendRequests: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  sentFriendRequests: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
