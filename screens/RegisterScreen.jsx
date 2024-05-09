c5b5d7bd810e7e4fa6732ea61dee79c005cd949d
meozalat2022 <meozalat2002@hotmail.com>
2024-05-09 09:59:14 +0300

    using android emulator IP instead of localhost/ and same port that us used in express

--- screens/RegisterScreen.jsx
+++ screens/RegisterScreen.jsx
@@ -28,7 +28,7 @@ const RegisterScreen = () => {
 
     // send a POST  request to the backend API to register the user
     axios
-      .post("http://192.168.1.45:8081/register", user)
+      .post("http://192.168.1.45:8000/register", user)
       .then((response) => {
         console.log(response);
         Alert.alert(
